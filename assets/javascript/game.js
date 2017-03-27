29924bd7c91d088b7749c4c415b95b38cac6a1c0
Jillian Flook <jkenw10@gmail.com>
2017-03-27 10:57:20 -0500

    added files for heroku deploy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,46 +8,83 @@ function renderButtons(){
 
 	//delete previous buttons to prevent duplicates
 	$("#buttonsArea").empty();
-	console.log("WORKING 123")
 
 	//loop through array to create buttons
 	for (i=0; i < animals.length; i++){
-		var button = $("<button>").addClass("animalBtn").attr("data-name", animals[i]).text(animals[i]);
+		var button = $("<button>").addClass("btn btn-default animalBtn").attr("data-name", animals[i]).text(animals[i]);
 		$("#buttonsArea").append(button);
 	}
 
-	console.log("AGAIN TESTING")
 }
 
 
 // on click event for adding button to array and buttons on page
 $("#addButton").on("click", function(event){
-	console.log("Testing user click")
 	//stops input button from trying to submit the input as defaulted
 	event.preventDefault();
 
 	//catches user input from form and adds it to the animals array
-	var userInput = $("#buttonInput").val().trim();
-	animals.push(userInput);
-
-	renderButtons();
-
+	var userInput = $("#buttonInput").val().trim().toLowerCase();
+	
+	//checking for duplicate buttons
+	if(animals.indexOf(userInput) === -1){
+		animals.push(userInput);
+		renderButtons();
+	}else{
+		return false};
 })
 
+// onclick of button, empty buttonsArea, add 10 videos of topic. 
+$("body").on("click", ".animalBtn", function() {
+    //gets name from button to include in the queryURL
+    var animalName = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      animalName + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+    $("#animalText").empty().text(animalName);
+
+    $("#gifArea").empty();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+     })
+    .done(function(response) {
+        var results = response.data;
+        for (var i = 0; i < 10; i++) {
+ 	       var gifDiv = $("<div class='item'>");
+
+           var rating = results[i].rating;
+
+           var p = $("<p>").text("Rating: " + rating);
+           
+           // prevent NSFW gifs
+           if(rating === "g" || rating === "pg"){
+	           var animalImg = $("<img>");
+	           animalImg.attr({"src": results[i].images.fixed_height_still.url, 
+	           	               "data-still": results[i].images.fixed_height_still.url,
+	           	               "data-animate": results[i].images.fixed_height.url,
+	           	               "data-state": "still",
+	           	           	   "class": "animalGif"});
+	           gifDiv.prepend(p, animalImg);
+
+	           $("#gifArea").prepend(gifDiv);
+       	    };
+        }
+    });
+}); //end of button on-click 
+
+$("#gifArea").on("mouseenter", ".animalGif", function(){
+	$(this).attr("src", $(this).attr("data-animate"));
+});
+
+$("#gifArea").on("mouseleave", ".animalGif", function(){
+	$(this).attr("src", $(this).attr("data-still"));
+});
 
 
 
 renderButtons();
-
-
-
-
-
-
-
-
-
 
 
 });// end of document ready
